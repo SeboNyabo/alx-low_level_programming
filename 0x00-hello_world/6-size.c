733805b4a765ee5b983e60fd1bc37ee465c9850d
SeboNyabo <eugenegitonga@protonmail.com>
2023-02-16 19:00:39 -0800

    low level language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ long long int c;
 char d;
 float f;
 
-printf("The size of a char: %lu byte(s)\n", (unsigned long)size of(d));
-	printf("size of an int: %lu byte(s)\n", (unsigned long)size of(a));
-	printf("size of long int: %lu byte(S)\n", (unsigned long)size of(b));
-	printf("size of long long int: %lu byte(s)\n", (unsigned long)size of(c);)
-	printf("size of float: %lu byte(s)\n", (unsigned long) size of(f);)
+	printf("The size of an int is: %lu.\n", (unsigned long)size of(d));
+	printf("size of an int: %lu.\n", (unsigned long)size of(a));
+	printf("size of long int: %lu.\n", (unsigned long)size of(b));
+	printf("size of long long int: %lu.\n", (unsigned long)size of(c);)
+	printf("size of float: %lu.\n", (unsigned long) size of(f));
 	return (0);
 
 }
