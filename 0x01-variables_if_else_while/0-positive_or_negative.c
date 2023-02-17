2919189fc1e54c9b8fec3ae40d72ffdf5a6027b8
SeboNyabo <eugenegitonga@protonmail.com>
2023-02-17 13:30:06 -0800

    Boolian exoressions

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* main-main block */
+
+/* main gives a random number to int/*
+ * it executes and prints it/*
+  * return (0)  */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+		printf("n is greater than zero");
+	else if (n < 0)
+		printf("n is less than 0");
+	else
+		printf("n is equal to zero");
+	return (0);
+}
