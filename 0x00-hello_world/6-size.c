50b2192c4f1da8a472047625b59b0a36eefa2738
SeboNyabo <eugenegitonga@protonmail.com>
2023-02-16 18:28:50 -0800

    low level languages

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
+printf("The size of a char: %lu byte(s)\n", (unsigned long)size of(d));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)size of(a));
+	printf("size of long int: %lu byte(S)\n", (unsigned long)size of(b));
+	printf("size of long long int: %lu byte(s)\n", (unsigned long)size of(c);)
+	printf("size of float: %lu byte(s)\n", (unsigned long) size of(f);)
+	return (0);
+
+}
