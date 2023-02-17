1fc30f527783b474bf6a2b640d7192cf56294b07
SeboNyabo <eugenegitonga@protonmail.com>
2023-02-16 18:44:37 -0800

    low level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main-program that prints a line with puts functuin
- * return: 0
+ * return: 0 (success)
  */
 int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle";
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
