281857249a67dab3e479b725b6e0fa7afcd4f6a8
SeboNyabo <eugenegitonga@protonmail.com>
2023-02-20 13:52:18 -0800

    low level language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,17 @@
 /** Prints the size of various types
  * main-entry point
  * return success 0 */
-int main (void)
+int main(void)
 {
-printf("The size of a char is %i bytes", 1); ("The size of in is %i", 4;)
-return (0)
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+	printf("Size of a char %lu byte(s)\n", (unsigned long)sizeof (d));
+	printf("Size of an int %lu byte(s)\n", (unsigned long)sizeof (a));
+	printf("Size of a long int %lu byte(s)\n", (unsigned long)sizeof (b));
+	printf("Size of a long long int %lu byte(s)\n", (unsigned long)sizeof (c));
+	printf("Size of a float %lu byte(s)\n", (unsigned long)sizeof (f));
+return (0);
 }
