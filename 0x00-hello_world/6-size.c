e9eef9cb6bde2461458abf4306d8529410a09c89
SeboNyabo <eugenegitonga@protonmail.com>
2023-02-20 09:12:56 -0800

    low level language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,9 @@
-#include<stdio.h>
-/**
- * main - entry point
- *
- * return: always 0 (success)
- */
-int main(void)
+#include <stdio.h>
+/** Prints the size of various types
+ * main-entry point
+ * return success 0 */
+int main (void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
-
-	printf("The size of an int is: %lu.\n", (unsigned long)size of(d));
-	printf("size of an int: %lu.\n", (unsigned long)size of(a));
-	printf("size of long int: %lu.\n", (unsigned long)size of(b));
-	printf("size of long long int: %lu.\n", (unsigned long)size of(c);)
-	printf("size of float: %lu.\n", (unsigned long) size of(f));
-	return (0);
-
+printf("The size of a char is %i bytes", 1); ("The size of in is %i", 4;)
+return (0)
 }
