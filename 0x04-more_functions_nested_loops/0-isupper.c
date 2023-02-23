4343f4d0cfedf410f67ca45716d81254705003b3
SeboNyabo <eugenegitonga@protonmail.com>
2023-02-23 08:07:40 -0800

    loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * isupper checks if a letter is uppercase
+ * returns 1 for uppercase and 0for lower case
+ */
+int _isupper(int c)
+
+{
+	char ch;
+
+	if (ch >= 65 && ch <= 90)
+			{
+			return (1);
+		}
+		return (0);
+}
