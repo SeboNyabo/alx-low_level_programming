b2722360414289a2bed896afff28f15a15119152
SeboNyabo <eugenegitonga@protonmail.com>
2023-02-23 09:07:42 -0800

    loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int mul(int a, int b)- multiplies two integers
+ * @a - is multiplied by b
+ * @b - is multiplied by a
+ * return the results
+ */
+int mul(int a, int b)
+{
+	int results;
+
+	results = a * b;
+		return (results);
+}
